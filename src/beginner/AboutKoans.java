d70175086cbf80acc4807f88b6a3dcac8bc4bd70
Jon-Erik <jonerik@pdx.edu>
2015-06-28 21:38:39 -0700

    first Koans commit

--- src/beginner/AboutKoans.java
+++ src/beginner/AboutKoans.java
@@ -7,13 +7,11 @@ import com.sandwich.koan.Koan;
 public class AboutKoans {
 
 	@Koan
-	public void findAboutKoansFile(){
-		fail("delete this line");
-	}
+
 	
 	@Koan
 	public void definitionOfKoanCompletion(){
-		boolean koanIsComplete = false;
+		boolean koanIsComplete = true;
 		if(!koanIsComplete){
 			fail("what if koanIsComplete was true?");
 		}
