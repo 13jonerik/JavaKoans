cb57ff59b812863c43fe4290177b4ea5cd9a7010
Jon-Erik <jonerik@pdx.edu>
2015-06-28 21:39:55 -0700

    first Koans commit

--- src/beginner/AboutKoans.java
+++ src/beginner/AboutKoans.java
@@ -5,8 +5,11 @@ import static com.sandwich.util.Assert.fail;
 import com.sandwich.koan.Koan;
 
 public class AboutKoans {
-	
 
+	@Koan
+	public void findAboutKoansFile(){
+		
+	}
 	
 	@Koan
 	public void definitionOfKoanCompletion(){
