094db64503326609ea23e995066406aeaeb5beec
Jon-Erik <jonerik@pdx.edu>
2015-07-01 17:56:52 -0700

    Koans commit AboutPrimitives3

--- src/beginner/AboutPrimitives.java
+++ src/beginner/AboutPrimitives.java
@@ -148,7 +148,7 @@ public class AboutPrimitives {
 	@Koan
 	public void primitivesOfTypeDoubleHaveAnObjectTypeDouble() {
 		Object number = 1.0;
-		assertEquals(getType(number), __);
+		assertEquals(getType(number), _);
 	}
 
 	@Koan
